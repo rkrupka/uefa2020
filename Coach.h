53c30b905643e76c3d7994bcedf1987afae8d828
rkrupka <61742823+rkrupka@users.noreply.github.com>
2021-04-18 14:05:37 +0200

    Create Coach.h

---
+++ Coach.h
@@ -0,0 +1,15 @@
+#ifndef COACH_H
+#define COACH_H
+//////////////////////////////////////////
+// Plik: Coach.h
+//////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+class Coach : public Person
+{
+  public:
+    int charisma;
+};
+#endif
+
